7ebbfe961ed36325b51c2ea471ef5700126e415b
Ian Proletov <yan.proletov@gmail.com>
2019-04-27 16:16:44 +0300

    2-st step index finish

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -4,7 +4,7 @@ import os from 'os';
 import path from 'path';
 import axios from 'axios';
 import httpAdapter from 'axios/lib/adapters/http';
-import loadPage, { getFileName } from '../src';
+import loadPage, { getName } from '../src';
 
 axios.defaults.adapter = httpAdapter;
 
@@ -13,7 +13,7 @@ describe.each([
   ['https://hexlet.io/courses', 'hexlet-io-courses.html'],
 ])(
   'convert %p', (address, expectedName) => {
-    const actualName = getFileName(address);
+    const actualName = getName(address, 'file');
     expect(actualName).toBe(expectedName);
   },
 );
@@ -26,7 +26,7 @@ test('Download', async () => {
     .replyWithFile(200, actualContentPath, { 'Content-type': 'text/html' });
   const outputDirPath = await fs.mkdtemp(`${os.tmpdir()}${path.sep}`);
   await loadPage('https://localhost/page', outputDirPath);
-  const outputPath = path.join(outputDirPath, getFileName('https://localhost/page'));
+  const outputPath = path.join(outputDirPath, getName('https://localhost/page', 'file'));
   const outputResult = await fs.readFile(outputPath, 'utf-8');
   const expectedResult = await fs.readFile(expectedContentPath, 'utf-8');
   expect(outputResult).toBe(expectedResult);
--- src/index.js
+++ src/index.js
@@ -3,19 +3,70 @@ import path from 'path';
 import url from 'url';
 import { trimEnd } from 'lodash';
 import { promises as fs } from 'fs';
+import cheerio from 'cheerio';
 import httpAdapter from 'axios/lib/adapters/http';
 
 axios.defaults.adapter = httpAdapter;
 
-export const getFileName = (pageAddress) => {
-  const urlAdd = url.parse(pageAddress);
-  const filename = `${urlAdd.host}${trimEnd(urlAdd.pathname, '/')}`.replace(/\W/g, '-');
-  return `${filename}.html`;
+const tags = {
+  script: 'src',
+  img: 'src',
+  link: 'href',
 };
 
+const loadedLinks = [];
+let localContent;
+
+const types = {
+  file: (pageAddress) => {
+    const urlAdd = url.parse(pageAddress);
+    const filename = `${urlAdd.host}${trimEnd(urlAdd.pathname, '/')}`.replace(/\W/g, '-');
+    return `${filename}.html`;
+  },
+  link: (pageAddress) => {
+    const { dir, name, ext } = path.parse(pageAddress);
+    return path.join(dir, name).replace(/\W/g, '-').concat(ext);
+  },
+  dir: pageAddress => types.file(pageAddress).replace(/\W/g, '-').concat('_files'),
+};
+
+export const getName = (pageAddress, type) => types[type](pageAddress);
+
 export default (pageAddress, outputpath) => {
-  const filepath = path.resolve(outputpath, getFileName(pageAddress));
+  const filepath = path.join(outputpath, getName(pageAddress, 'file'));
+  const linkDir = getName(pageAddress, 'dir');
+  let absoluteLinkPath = '';
   return axios.get(pageAddress)
-    .then(response => fs.writeFile(filepath, response.data))
-    .catch(e => console.error(e.message));
+    .then((response) => {
+      const $ = cheerio.load(response.data);
+      const listOfTags = Object.keys(tags);
+      listOfTags.forEach((tag) => {
+        const attribute = tags[tag];
+        $(tag).each((index, currentTag) => {
+          const link = $(currentTag).attr(attribute);
+          if (link && !url.parse(link).host) {
+            const linkName = getName(link, 'link');
+            const linkPath = path.join(linkDir, linkName);
+            loadedLinks.push(link);
+            $(currentTag).attr(attribute, linkPath);
+          }
+        });
+      });
+      localContent = $.html();
+      return fs.mkdir(path.join(outputpath, linkDir));
+    })
+    .then(() => {
+      loadedLinks.forEach((link) => {
+        const linkName = getName(link, 'link');
+        const linkPath = path.join(linkDir, linkName);
+        absoluteLinkPath = path.join(outputpath, linkPath, linkName);
+        return axios.get(url.resolve(pageAddress, link))
+          .then(linkResponse => fs.writeFile(absoluteLinkPath, linkResponse.data));
+      });
+    })
+    .then(() => fs.writeFile(filepath, localContent))
+    .catch((error) => {
+      console.log('error');
+      throw new Error(error);
+    });
 };
